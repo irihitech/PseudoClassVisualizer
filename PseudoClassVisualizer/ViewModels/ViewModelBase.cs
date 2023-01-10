0ef0c77f60629ee6d9e784f94458f73457a7fb5a
rabbitism <popmessiah@hotmail.com>
2023-01-10 14:18:10 +0800

    misc: initialize project.

---
+++ PseudoClassVisualizer/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace PseudoClassVisualizer.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
---
+++ PseudoClassVisualizer/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace PseudoClassVisualizer.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
